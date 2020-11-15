1fb2cab543895f74d30d6a24f4a6c944bfb84782
Jefferson Gouveia <jeffersongouveia@eucatur.com.br>
2020-11-15 18:32:51 -0400

    ✅ Personalized the appointment success page

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,11 +1,42 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useCallback, useMemo } from 'react'
+import Icon from 'react-native-vector-icons/Feather'
+import { useNavigation, useRoute } from '@react-navigation/native'
+import { format } from 'date-fns'
+
+import { Container, Description, OkButton, OkButtonText, Title } from './styles'
+
+interface RouteParams {
+  date: number
+}
 
 const AppointmentCreated: React.FC = () => {
+  const { reset } = useNavigation()
+  const route = useRoute()
+  const params = route.params as RouteParams
+
+  const handleOkButtonPressed = useCallback(() => {
+    reset({
+      routes: [{ name: 'Dashboard' }],
+      index: 0,
+    })
+  }, [reset])
+
+  const selectedDateFormatted = useMemo(() => {
+    const selectedDate = new Date(params.date)
+    return format(selectedDate, "EEEE',' LLLL dd',' uu 'at' h aaaa")
+  }, [params.date])
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text style={{ color: '#fff', fontSize: 24 }}>Appointment created</Text>
-    </View>
+    <Container>
+      <Icon name="check" size={80} color="#04d361" />
+
+      <Title>Appointment done</Title>
+      <Description>{selectedDateFormatted}</Description>
+
+      <OkButton onPress={handleOkButtonPressed}>
+        <OkButtonText>OK</OkButtonText>
+      </OkButton>
+    </Container>
   )
 }
 
